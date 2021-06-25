4cf6d58b38025fcd3e59f3c06ddd25b49c5024b9
Alex <dr.kittykat77@gmail.com>
2021-06-25 00:37:23 -0600

    Various code changes, dotenv for security, and docs

--- botmain.py
+++ botmain.py
@@ -5,31 +5,37 @@ from discord_slash import SlashCommand
 from dotenv import load_dotenv
 
 load_dotenv()
-dotenv_config = load_dotenv(".env")
 
 client = discord.Client()
 slash = SlashCommand(client, sync_commands=True) # Declares slash commands through the client.
+
 if not discord.opus.is_loaded():
-    discord.opus.load_opus('/home/container/lib/libopus.so')
+    discord.opus.load_opus('/home/container/lib/libopus.so') # This is a linux specific solution; change this to the path of the libopus library (can be download from https://opus-codec.org/downloads/ or built from source) if working in windows
 if discord.opus.is_loaded():
     print("Opus loaded successfully!")
 else:
-    print("Opus did not properly load. Should still be able to play in non-opus VCs.")
+    print("Opus did not properly load. Should still be able to play in non-opus VCs.") # This should be fine. There is sometimes an issue with joining a vc
 
 @client.event
 async def on_ready():
     print(f'{client.user} successfully connected!')
 
-guild_ids = dotenv_config["GUILD_IDS"]
-
+guild_ids = [int(x) for x in os.environ.get("GUILD_IDS").split(" ")] # Parses and adds all the server ids from .env to a list of servers for each slash command to work
 
 @slash.slash(name="ping", guild_ids=guild_ids)
-async def _ping(ctx_ping): # Defines a new "context" (ctx) command called "ping."
+async def _ping(ctx_ping): 
+    '''
+    Simple ping command to see if the latency and if the bot is dead
+    '''
+    print("Ping command recieved")
     await ctx_ping.send(f"Pong! ({client.latency*1000}ms)")
 
 
 @slash.slash(name="indie", guild_ids=guild_ids)
 async def _indie(ctx_indie):
+    '''
+    Have Dr_KittyKat II join VC and start playing Indie 102.3
+    '''
     if ctx_indie.author.voice and ctx_indie.author.voice.channel:
         channel = ctx_indie.author.voice.channel
         vc = await channel.connect()
@@ -46,14 +52,20 @@ async def _indie(ctx_indie):
         await ctx_indie.send(embed=msgembed)
 
 
-@slash.slash(name="nowplaying", guild_ids=guild_ids)
+@slash.slash(name="now playing", guild_ids=guild_ids)
 async def _nowplaying(ctx_nowplaying):
+    '''
+    Get the current playing song, artist, and DJ
+    '''
     msgembed = discord.Embed(title="Now Playing on KVOQ, indie 102.3", thumbnail="/home/kitty/Desktop/kvoq-bot/indie1023-2-Clr-Circle-Logo-Print-201907.png", description=get_track_info(), color=0xF887D7)
     await ctx_nowplaying.send(embed=msgembed)
 
 
 @slash.slash(name="disconnect", guild_ids=guild_ids)
 async def _disconnect(ctx_disconnect):
+    '''
+    Have Dr_KittyKat II disconnect from the VC
+    '''
     for x in client.voice_clients:
         if(x.guild == ctx_disconnect.guild):
             x.stop()
@@ -66,10 +78,18 @@ async def _disconnect(ctx_disconnect):
 
 
 def get_track_info():
+    '''
+    Command to make 2 lines of info for /indie and /nowplaying in the format:
+    
+    [Title] by [Artist]
+    DJ [dj name]
+    
+    This can be easily expanded by finding your own info from the exposed json file at https://playlist.cprnetwork.org/won_plus3/KVOQ.json
+    '''
     with urllib.request.urlopen("https://playlist.cprnetwork.org/won_plus3/KVOQ.json") as track_info_json_url:
         track_info_json = json.loads(track_info_json_url.read().decode())
         track_info = track_info_json[0]["title"] + " by " + track_info_json[0]["artist"] + "\n" + "DJ " + track_info_json[0]["line_2"]
     return track_info
 
-client.run(dotenv_config["TOKEN"]) #PRIVATE DISCORD TOKEN GOES HERE BE VERY CAREFUL WHEN REDISTRIBUTING
+client.run(os.environ["TOKEN"])
 # permissions: 2150697984
