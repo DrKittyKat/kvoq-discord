a14ad9e2d704a74d8f52c4d5731362b45faffd5a
Dr_KittyKat <dr.kittykat77@gmail.com>
2023-06-17 20:18:56 -0600

    Fixed so that it kinda works with new discord. No slash commands :c

--- botmain.py
+++ botmain.py
@@ -7,15 +7,34 @@ import string
 import json
 import urllib
 import os
-from discord_slash import SlashCommand
+import enum
+# from discord_slash import SlashCommand
 from dotenv import load_dotenv
+
+class CommandType(enum.Enum):
+    slash = 1
+    text = 2
+class DiscordCommand:
+    def __init__(self, cmdtype: CommandType, context):
+        self.cmdtype = cmdtype
+        self.context = context
+    
+    def get_cmd_ctx(self):
+        if (self.cmdtype == CommandType.slash):
+            return self.context.author
+        elif (self.cmdtype == CommandType.text):
+            return self.context.author
 
 load_dotenv()
 
-client = discord.Client()
-slash = SlashCommand(client, sync_commands=True) # Declares slash commands through the client.
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+# slash = SlashCommand(client, sync_commands=True) # Declares slash commands through the client.
 msg_prefix = "nya-"
+guild_ids = [int(x) for x in os.environ.get("GUILD_IDS").split(" ")] # Parses and adds all the server ids from .env to a list of servers for each slash command to work
 
+print("loading opus")
 if not discord.opus.is_loaded():
     discord.opus.load_opus('/home/container/lib/libopus.so') # This is a linux specific solution; change this to the path of the libopus library (can be download from https://opus-codec.org/downloads/ or built from source) if working in windows
 if discord.opus.is_loaded():
@@ -26,107 +45,99 @@ else:
 @client.event
 async def on_ready():
     print(f'{client.user} successfully connected!')
-
-guild_ids = [int(x) for x in os.environ.get("GUILD_IDS").split(" ")] # Parses and adds all the server ids from .env to a list of servers for each slash command to work
-
-@slash.slash(name="ping", guild_ids=guild_ids)
-async def _ping(ctx_ping): 
-    '''
-    Simple ping command to see if the latency and if the bot is dead
-    '''
-    print("Ping command recieved")
-    await ctx_ping.send(f"Pong! ({client.latency*1000}ms)")
-
+    
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
     
+    text_command = DiscordCommand(CommandType.text, message)
     
     if message.content == msg_prefix + "ping":
-        print("Ping command recieved")
-        await message.channel.send(f"Pong! ({client.latency*1000}ms)")
-
+        await bot_ping(text_command)
 
     if message.content == msg_prefix + "indie":
-        if message.author.voice and message.author.voice.channel:
-            channel = message.author.voice.channel
-            vc = await channel.connect()
-            audio_source = discord.FFmpegPCMAudio(source='http://stream1.cprnetwork.org/cpr3_lo')
-            vc.play(audio_source)
-            msgembed = discord.Embed(title="You are listening to KVOQ, indie 102.3", thumbnail="/home/kitty/Desktop/kvoq-bot/indie1023-2-Clr-Circle-Logo-Print-201907.png", description=get_track_info(), color=0xF887D7)
-            await message.channel.send(embed=msgembed)
-            while not len(channel.members) <= 1:
-                await asyncio.sleep(30)
-            await vc.disconnect()
-        else:
-            msgembed = discord.Embed(description="You are not connected to any voice channel...", color=0xF887D7)
-            await message.channel.send(embed=msgembed)
-    
+        await bot_indie(text_command)
     
     if message.content == msg_prefix + "nowplaying":
-        msgembed = discord.Embed(title="Now Playing on KVOQ, indie 102.3", thumbnail="/home/kitty/Desktop/kvoq-bot/indie1023-2-Clr-Circle-Logo-Print-201907.png", description=get_track_info(), color=0xF887D7)
-        await message.channel.send(embed=msgembed)
-    
+        await bot_nowplaying(text_command)
     
     if message.content == msg_prefix + "disconnect":
-        for x in client.voice_clients:
-            if(x.guild == message.guild):
-                x.stop()
-                await x.disconnect()
-                msgembed = discord.Embed(description="Disconnected!", color=0xF887D7)
-                return await message.channel.send(embed=msgembed)
+        await bot_disconnect(text_command)
 
-        msgembed = discord.Embed(description="I'm not connected to any voice channel!", color=0xF887D7)
-        return await message.channel.send(embed=msgembed)
-
-
+'''
+@slash.slash(name="ping", guild_ids=guild_ids)
+async def _ping(ctx_ping): 
+    slash_command = DiscordCommand(CommandType.slash, ctx_ping)
+    await bot_ping(slash_command)
+    await ctx_ping.send("")
 
 @slash.slash(name="indie", guild_ids=guild_ids)
 async def _indie(ctx_indie):
+    slash_command = DiscordCommand(CommandType.slash, ctx_indie)
+    await bot_indie(slash_command)
+    await ctx_indie.send(" ")
+
+@slash.slash(name="nowplaying", guild_ids=guild_ids)
+async def _nowplaying(ctx_nowplaying):
+    slash_command = DiscordCommand(CommandType.slash, ctx_nowplaying)
+    await bot_nowplaying(slash_command)
+    await ctx_nowplaying.send(" ")
+
+    
+@slash.slash(name="disconnect", guild_ids=guild_ids)
+async def _disconnect(ctx_disconnect):
+    slash_command = DiscordCommand(CommandType.slash, ctx_disconnect)
+    await bot_disconnect(slash_command)
+    await ctx_disconnect.send(" ")
+'''
+async def bot_ping(CommandClass : DiscordCommand):
+    '''
+    Simple ping command to see if the latency and if the bot is dead
+    '''
+    print("Ping command recieved")
+    await CommandClass.context.channel.send(f"Pong! ({client.latency*1000}ms)")
+
+async def bot_indie(CommandClass : DiscordCommand):
     '''
     Have Dr_KittyKat II join VC and start playing Indie 102.3
     '''
-    if ctx_indie.author.voice and ctx_indie.author.voice.channel:
-        channel = ctx_indie.author.voice.channel
+    if CommandClass.context.author.voice and CommandClass.context.author.voice.channel:
+        channel = CommandClass.context.author.voice.channel
         vc = await channel.connect()
         audio_source = discord.FFmpegPCMAudio(source='http://stream1.cprnetwork.org/cpr3_lo')
         vc.play(audio_source)
         msgembed = discord.Embed(title="You are listening to KVOQ, indie 102.3", thumbnail="/home/kitty/Desktop/kvoq-bot/indie1023-2-Clr-Circle-Logo-Print-201907.png", description=get_track_info(), color=0xF887D7)
-        await ctx_indie.send(embed=msgembed)
+        await CommandClass.context.channel.send(embed=msgembed)
         while not len(channel.members) <= 1:
             await asyncio.sleep(30)
-        #vc.stop()
         await vc.disconnect()
     else:
         msgembed = discord.Embed(description="You are not connected to any voice channel...", color=0xF887D7)
-        await ctx_indie.send(embed=msgembed)
+        await CommandClass.context.channel.send(embed=msgembed)
 
-
-@slash.slash(name="nowplaying", guild_ids=guild_ids)
-async def _nowplaying(ctx_nowplaying):
+async def bot_nowplaying(CommandClass : DiscordCommand):
     '''
     Get the current playing song, artist, and DJ
     '''
-    msgembed = discord.Embed(title="Now Playing on KVOQ, indie 102.3", thumbnail="/home/kitty/Desktop/kvoq-bot/indie1023-2-Clr-Circle-Logo-Print-201907.png", description=get_track_info(), color=0xF887D7)
-    await ctx_nowplaying.send(embed=msgembed)
+    msgembed = discord.Embed(title="Now Playing on KVOQ, indie 102.3", description=get_track_info(), color=0xF887D7)
+    urlThumbnail = "https://pbs.twimg.com/profile_images/1145771354244993024/rEN4VY4L_400x400.png"
+    msgembed.set_thumbnail(url=urlThumbnail)
+    await CommandClass.context.channel.send(embed=msgembed)
 
-
-@slash.slash(name="disconnect", guild_ids=guild_ids)
-async def _disconnect(ctx_disconnect):
+async def bot_disconnect(CommandClass : DiscordCommand):
     '''
     Have Dr_KittyKat II disconnect from the VC
     '''
     for x in client.voice_clients:
-        if(x.guild == ctx_disconnect.guild):
+        if(x.guild == CommandClass.context.guild):
             x.stop()
             await x.disconnect()
             msgembed = discord.Embed(description="Disconnected!", color=0xF887D7)
-            return await ctx_disconnect.send(embed=msgembed)
+            return await CommandClass.context.channel.send(embed=msgembed)
     
     msgembed = discord.Embed(description="I'm not connected to any voice channel!", color=0xF887D7)
-    return await ctx_disconnect.send(embed=msgembed)
-
+    return await CommandClass.context.channel.send(embed=msgembed)
 
 def get_track_info():
     '''
